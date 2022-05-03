b3e88a1c9306569dc2e54f590110123fcc6951de
Daulet Nassipkali <uralskdev@gmail.com>
2022-05-03 11:00:41 +0300

    Delete backend2 directory

--- backend2/routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const userRouter = require('./userRouter')
-const adminRouter = require('./adminRouter')
-
-router.use('/user', userRouter)
-router.use('/admin', adminRouter)
-
-module.exports = router
